b18df111041a8f368d0768c59647146cd307d55b
Prakhar Shrivastava <prkhr@prkhr.local>
2026-07-04 18:20:53 +0530

    cors config

--- src/main/java/JobPortal/SpringJobPortal/Security/CorsConfig.java
+++ src/main/java/JobPortal/SpringJobPortal/Security/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
                 "http://localhost:5173",
                 "http://localhost:3000",
                 "https://talent-sync-ui.vercel.app",
-                "https://hire-hub-frontend-mu.vercel.app"
+                "https://hire-hub-frontend-mu.vercel.app",
+                "https://talent-sync-ui.vercel.app/"
         ));
 
         configuration.setAllowedMethods(List.of(
